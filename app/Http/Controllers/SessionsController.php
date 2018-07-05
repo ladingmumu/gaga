cb58e467d633402af20f1d65838e06b47a65511c
ladingmumu <295741509@qq.com>
2018-07-05 14:00:47 +0000

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    //显示登录页面
+    public function create(){
+        return view('sessions.create');
+    }
+
+    //登录
+    public function store(Request $request){
+        $data = $this->validate($request, [
+               'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($data, $request->has('remember'))) {
+            //身份验证通过
+            session()->flash('success','登录成功，欢迎回来');
+            return redirect()->route('users.show',[Auth::user()]);
+        } else{
+            //身份验证失败
+            session()->flash('danger','邮箱和密码不匹配');
+            return redirect()->back();
+        }
+        return;
+    }
+
+    //退出登录
+    public function destroy(){
+        Auth::logout();
+        session()->flash('success','您已经退出登录');
+        return redirect()->route('login');
+    }
+
+}
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,8 +4,31 @@
             <a href="{{ route('home') }}" id="logo">GaGa</a>
             <nav>
                 <ul class="nav navbar-nav navbar-right">
+                @if (Auth::check())
+                    <li><a href="#">所有用户</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            {{ Auth::user()->name }} <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{ route('users.show', Auth::user()->id) }}">个人中心</a></li>
+                            <li><a href="#">资料更新</a></li>
+                            <li class="divider"></li>
+                            <li>
+                                <a id="logout" href="{{ route('logout') }}">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        <button type="submit" class="btn btn-danger btn-block" name="button">退出</button>
+                                    </form>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                @else
                     <li><a href="{{ route('help') }}">帮助</a></li>
-                    <li><a href="#">登录</a></li>
+                    <li><a href="{{ route('login') }}">登录</a></li>
+                @endif
                 </ul>
             </nav>
         </div>
