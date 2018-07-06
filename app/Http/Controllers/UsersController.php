04c550a904ef2a7d21c255606902dbd662b98dbf
ladingmumu <295741509@qq.com>
2018-07-06 15:14:02 +0000

    Add password resets & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -57,13 +57,11 @@ class UsersController extends Controller
     protected function sendEmailConfirmationTo($user){
             $view = 'emails.confirm';
             $data = compact('user');
-            $from = '295741509@qq.com';
-            $name = 'qianqian';
               $to = $user->email;
          $subject = '感谢注册 GAGA 应用！请确认您的邮箱。';
 
-         Mail::send($view, $data, function($message) use ($from, $name, $to, $subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+         Mail::send($view, $data, function($message) use ($to, $subject){
+            $message->to($to)->subject($subject);
          });
     }
 
